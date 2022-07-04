8344c274e5c3dd23b6a92ca044c6867d5ae65613
Samara Soares Ferreira <samara.sofe@gmail.com>
2022-07-04 18:08:04 -0300

    Pacote Styled-Components add

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+function Home() {
+    return (
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+                <li>
+                        <img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+                <li>
+                    <img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+            </ul>
+        </div>
+        
+    )
+
+}
+
+export default Home;
